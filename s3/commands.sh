51a8041a9c1146003fada34a49b8631b575895e3
Thong Van <thong@coffeemugvn.com>
2024-12-26 14:52:55 +0700

    Add a loop to retry credential retrieval every 5 seconds until they are available, enhancing reliability during stack setup

--- s3/commands.sh
+++ s3/commands.sh
@@ -11,17 +11,22 @@ if [ $? -ne 0 ]; then
     echo "Failed to create stack"
     exit 1
 fi
-sleep 30
-# Store all credentials in variables
-ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name s3-backup-setup --query 'Stacks[0].Outputs[?OutputKey==`AccessKeyId`].OutputValue' --output text)
-SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name s3-backup-setup --query 'Stacks[0].Outputs[?OutputKey==`SecretAccessKey`].OutputValue' --output text)
-BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name s3-backup-setup --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue' --output text)
-USER_ARN=$(aws cloudformation describe-stacks --stack-name s3-backup-setup --query 'Stacks[0].Outputs[?OutputKey==`UserArn`].OutputValue' --output text)
 
-# Print the values
-cat << EOF
+while [ -z "${ACCESS_KEY_ID}" ]; do
+    sleep 5
+    # Store all credentials in variables
+    ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name s3-backup-setup --query 'Stacks[0].Outputs[?OutputKey==`AccessKeyId`].OutputValue' --output text)
+    SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name s3-backup-setup --query 'Stacks[0].Outputs[?OutputKey==`SecretAccessKey`].OutputValue' --output text)
+    BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name s3-backup-setup --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue' --output text)
+    USER_ARN=$(aws cloudformation describe-stacks --stack-name s3-backup-setup --query 'Stacks[0].Outputs[?OutputKey==`UserArn`].OutputValue' --output text)
+
+    # Print the values
+    cat << EOF
 ${BUCKET_NAME}:
   AccessKeyId: ${ACCESS_KEY_ID}
   SecretAccessKey: ${SECRET_ACCESS_KEY}
   UserArn: ${USER_ARN}
 EOF
+done
+
+
